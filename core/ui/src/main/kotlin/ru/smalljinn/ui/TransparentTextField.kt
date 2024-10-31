ffe137f62e4709dabea0c6a3a4480809ff35144a
vladislav <m.d.grinko@gmail.com>
2024-10-31 16:27:20 +0700

    Animated size at TransparentTextField

--- core/ui/src/main/kotlin/ru/smalljinn/ui/TransparentTextField.kt
+++ core/ui/src/main/kotlin/ru/smalljinn/ui/TransparentTextField.kt
@@ -1,5 +1,6 @@
 package ru.smalljinn.ui
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,15 +10,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 
-const val MAX_EDITABLE_DESCRIPTION_TEXT_FIELD_LINES = 4
+private const val MAX_EDITABLE_DESCRIPTION_TEXT_FIELD_LINES = 4
 
 @Composable
 fun TransparentTextField(
@@ -31,13 +37,13 @@ fun TransparentTextField(
     imeAction: ImeAction = ImeAction.Default
 ) {
     val contentColor = LocalContentColor.current
+    var maxLines by rememberSaveable { mutableIntStateOf(maxLines(readOnly)) }
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         TextField(
             colors = TextFieldDefaults.colors(
                 focusedIndicatorColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent,
                 disabledIndicatorColor = Color.Transparent,
-
             ),
             value = text,
             onValueChange = onTextChanged,
@@ -47,9 +53,12 @@ fun TransparentTextField(
                 imeAction = imeAction,
                 capitalization = KeyboardCapitalization.Sentences
             ),
-            maxLines = if (!readOnly) MAX_EDITABLE_DESCRIPTION_TEXT_FIELD_LINES else Int.MAX_VALUE,
+            maxLines = maxLines,
             shape = RoundedCornerShape(16.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .onGloballyPositioned { maxLines = maxLines(readOnly) }
+                .fillMaxWidth()
+                .animateContentSize()
         )
         hintText?.let {
             if (shouldShowHint) {
@@ -57,4 +66,10 @@ fun TransparentTextField(
             }
         }
     }
+}
+
+private fun maxLines(readOnly: Boolean) = if (!readOnly) {
+    MAX_EDITABLE_DESCRIPTION_TEXT_FIELD_LINES
+} else {
+    Int.MAX_VALUE
 }
