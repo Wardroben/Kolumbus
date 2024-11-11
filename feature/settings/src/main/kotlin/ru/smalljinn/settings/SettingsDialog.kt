fef87529047fe8a409c6cfe1945fcf220283772a
vladislav <m.d.grinko@gmail.com>
2024-11-11 16:25:11 +0700

    places backup settings preparation

--- feature/settings/src/main/kotlin/ru/smalljinn/settings/SettingsDialog.kt
+++ feature/settings/src/main/kotlin/ru/smalljinn/settings/SettingsDialog.kt
@@ -1,5 +1,8 @@
 package ru.smalljinn.settings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -13,8 +16,11 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,8 +29,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,7 +44,9 @@ fun SettingsDialog(onDismiss: () -> Unit, viewModel: SettingsViewModel = hiltVie
     SettingsDialog(
         onDismiss = onDismiss,
         settingsUiState = settings,
-        onChangeCardStyle = viewModel::updateCardStyle
+        onChangeCardStyle = viewModel::updateCardStyle,
+        onImportBackupClicked = { TODO() },
+        onCreateBackupClicked = { TODO() }
     )
 }
 
@@ -44,7 +54,9 @@ fun SettingsDialog(onDismiss: () -> Unit, viewModel: SettingsViewModel = hiltVie
 fun SettingsDialog(
     onDismiss: () -> Unit,
     settingsUiState: SettingsUiState,
-    onChangeCardStyle: (Boolean) -> Unit
+    onChangeCardStyle: (Boolean) -> Unit,
+    onCreateBackupClicked: () -> Unit,
+    onImportBackupClicked: () -> Unit
 ) {
     val configuration = LocalConfiguration.current
 
@@ -61,7 +73,9 @@ fun SettingsDialog(
                     is SettingsUiState.Success -> {
                         SettingsPanel(
                             settingsUiState.settings,
-                            onChangeCardStyle = onChangeCardStyle
+                            onChangeCardStyle = onChangeCardStyle,
+                            onImportBackupClicked = onImportBackupClicked,
+                            onCreateBackupClicked = onCreateBackupClicked
                         )
                     }
                 }
@@ -78,12 +92,65 @@ fun SettingsDialog(
 @Composable
 private fun ColumnScope.SettingsPanel(
     settings: SettingsEditable,
-    onChangeCardStyle: (Boolean) -> Unit
+    onChangeCardStyle: (Boolean) -> Unit,
+    onImportBackupClicked: () -> Unit,
+    onCreateBackupClicked: () -> Unit
 ) {
     SettingSectionTitle(stringResource(R.string.place_card_style_setting))
     Column(Modifier.selectableGroup()) {
-        SettingChooserRow(stringResource(R.string.full_placecard_style), selected = !settings.useCompactStyle) { onChangeCardStyle(false) }
-        SettingChooserRow(stringResource(R.string.compact_placecard_style), selected = settings.useCompactStyle) { onChangeCardStyle(true) }
+        SettingChooserRow(
+            stringResource(R.string.full_placecard_style),
+            selected = !settings.useCompactStyle
+        ) { onChangeCardStyle(false) }
+        SettingChooserRow(
+            stringResource(R.string.compact_placecard_style),
+            selected = settings.useCompactStyle
+        ) { onChangeCardStyle(true) }
+    }
+    SettingSectionTitle("Places backup")
+    BackupButtonRow(text = "Export backup as file", isExport = true) { onCreateBackupClicked() }
+    BackupButtonRow(text = "Import backup as file", isExport = false) { onImportBackupClicked() }
+}
+
+@Composable
+private fun BackupButtonsRow(
+    modifier: Modifier = Modifier,
+    onCreateBackupClicked: () -> Unit,
+    onImportBackupClicked: () -> Unit
+) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+        OutlinedButton(onClick = onImportBackupClicked) {
+            Text("Import")
+        }
+        Button(onClick = onCreateBackupClicked) {
+            Text("Create")
+        }
+    }
+}
+
+@Composable
+private fun BackupButtonRow(
+    text: String,
+    isExport: Boolean,
+    onClick: () -> Unit
+) {
+    Box(Modifier
+        .fillMaxWidth()
+        .clickable(role = Role.Button) { onClick() },
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        Row(Modifier.padding(12.dp)) {
+            if (isExport) Icon(
+                painter = painterResource(R.drawable.baseline_upload_24),
+                contentDescription = "Export backup"
+            )
+            else Icon(
+                painter = painterResource(R.drawable.baseline_download_24),
+                contentDescription = "Import backup"
+            )
+            Spacer(Modifier.width(12.dp))
+            Text(text)
+        }
     }
 }
 
@@ -112,4 +179,16 @@ private fun SettingChooserRow(
         Spacer(Modifier.width(10.dp))
         Text(text)
     }
+}
+
+@Preview
+@Composable
+private fun SettingsDialogPreview() {
+    SettingsDialog(
+        onDismiss = {},
+        settingsUiState = SettingsUiState.Success(SettingsEditable(useCompactStyle = true)),
+        onImportBackupClicked = {},
+        onCreateBackupClicked = {},
+        onChangeCardStyle = {}
+    )
 }
