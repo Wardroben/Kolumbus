1fbb8a1c3c0ca296c8b3d7e5c61bb0616f06c526
vladislav <m.d.grinko@gmail.com>
2024-11-22 11:50:22 +0700

    Import intent filter

--- app/src/main/java/ru/smalljinn/kolumbus/MainActivity.kt
+++ app/src/main/java/ru/smalljinn/kolumbus/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.smalljinn.kolumbus
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -15,22 +18,34 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.smalljinn.kolumbus.navigation.KolumbusNavHost
 import ru.smalljinn.kolumbus.ui.rememberKolumbusAppState
 import ru.smalljinn.kolumbus.ui.theme.KolumbusTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val importViewModel: RootComponent by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        importPlaces(intent, onImport = { uri -> importViewModel.importPlaces(uri) })
+
         enableEdgeToEdge()
         setContent {
             val appState = rememberKolumbusAppState()
-            val context = LocalContext.current
+            val scope = rememberCoroutineScope()
             KolumbusTheme {
                 Scaffold(
                     contentWindowInsets = WindowInsets(0, 0, 0, 0),
@@ -45,6 +60,16 @@ class MainActivity : ComponentActivity() {
                                 WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)
                             )
                     ) {
+                        ObserveAsEvents(flow = importViewModel.uiEvent) { event ->
+                            val text = when(event) {
+                                is ImportUiEvent.Error -> event.message
+                                is ImportUiEvent.Imported -> event.message
+                            }
+                            scope.launch {
+                                appState.snackbarHostState.showSnackbar(message = text)
+                            }
+                            //Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+                        }
                         //Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
                         KolumbusNavHost(
                             appState = appState,
@@ -58,6 +83,33 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 }
+            }
+        }
+    }
+
+    private fun importPlaces(intent: Intent?, onImport: (Uri) -> Unit) {
+        val importType = intent?.type == "application/octet-stream"
+
+        if (importType && intent != null) {
+            when (intent.action) {
+                Intent.ACTION_SEND,
+                Intent.ACTION_VIEW -> {
+                    val backupFileUri = intent.data
+                    backupFileUri?.let { onImport(it) } ?: return
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun <T> ObserveAsEvents(flow: Flow<T>, onEvent: (T) -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(flow, lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
             }
         }
     }
---
+++ app/src/main/java/ru/smalljinn/kolumbus/RootComponent.kt
@@ -0,0 +1,55 @@
+package ru.smalljinn.kolumbus
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import ru.smalljinn.domain.R
+import ru.smalljinn.domain.repository.ImportExportRepository
+import ru.smalljinn.domain.resource.ResourceManager
+import ru.smalljinn.model.data.response.ImportError
+import ru.smalljinn.model.data.response.Result
+import javax.inject.Inject
+
+@HiltViewModel
+class RootComponent @Inject constructor(
+    private val importExportRepository: ImportExportRepository,
+    private val resourceManager: ResourceManager
+): ViewModel() {
+    private val _uiEvent = Channel<ImportUiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun importPlaces(uri: Uri) {
+        viewModelScope.launch {
+            when(val importResult = importExportRepository.importPlaces(uri)) {
+                is Result.Error -> processErrorMessage(importResult.error)
+                is Result.Success -> showSuccessMessage()
+            }
+        }
+    }
+
+    private fun processErrorMessage(importError: ImportError) {
+        val message = when(importError) {
+            ImportError.FILE_NOT_FOUND -> resourceManager.getString(R.string.file_not_found_import_error)
+            ImportError.NOT_BACKUP_FILE -> resourceManager.getString(R.string.not_backup_file_import_error)
+            ImportError.UNKNOWN -> resourceManager.getString(R.string.unknown_import_error)
+        }
+        showMessage(ImportUiEvent.Error(message))
+    }
+
+    private fun showSuccessMessage() {
+        showMessage(ImportUiEvent.Imported(resourceManager.getString(R.string.import_success)))
+    }
+
+    private fun showMessage(event: ImportUiEvent) {
+        _uiEvent.trySend(event)
+    }
+}
+
+sealed class ImportUiEvent {
+    data class Imported(val message: String): ImportUiEvent()
+    data class Error(val message: String): ImportUiEvent()
+}
